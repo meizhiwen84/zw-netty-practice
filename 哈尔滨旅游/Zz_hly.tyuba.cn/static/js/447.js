24a1768489fda4631fb8c979f47b45778c2d08ee
meizhiwen <meizhiwen84@163.com>
2020-10-24 14:33:40 +0800

    哈尔滨

---
+++ 哈尔滨旅游/Zz_hly.tyuba.cn/static/js/447.js
@@ -0,0 +1,17 @@
+/* 
+ * 2020-10-22 07:32:09
+ * 本客服系统由张家界鼎尖网络科技有限公司开发，已申请计算机软件著作权。
+ * 受《国家计算机软件著作权法》保护，禁止任何形式的抄袭盗用，否则我们将依法追究相关法律责任。
+ * 合作电话：0744-8383800 网址：www.itzjj.com  法律顾问：代可律师
+ * 功能列表：
+ * 1、客服智能随机展示，一个页面可设置多个客服，访客访问会随机展现一个；
+ * 2、客服展现智能锁定：访客访问，展示A客服后，不管刷新，还是分享链接给微信好友，都是显示A客服；
+ * 3、手机，电脑，小程序，快速控制客服上下线；
+ * 4、客服24小时自动排班上下线；
+ * 5、客服可指定手机端和电脑端分开展现；
+ * 6、客服信息加密，防止客服资料泄露，加密内容如下：
+ */
+var $_STORAGENAME = "1563331746";
+var $_KFDEBUG = "0";
+var $_KFLOCK = "1";
+var $_KFINFO = "lE0/xugXoZNM6Lf0YYteCv75Pc3xLqmGGw5CSoT6QH4JIUV5FS4b+zQBq7Pr2xWtFnWtYZWSQEgX1aJmD+TlBRDgWnwuD2SZFU2HAVR6Yhmwzr/Fn7yshNB187TaDtTImXHbw11UTqVKIowegV0db1q1WwqKpJL6x/EmQemcFU3s0TUNf7ciAiJGnDRtWyIHXKQyyyjr+dRyVwui2dAkCM87d9JYl9QTSXS5kV97urML7nNJVRmAgGDh89dVJyhZ2vtwzCioGmYaDrmChE5FrCiKGrFgK3Ug2L8iLZRAdmvzx/jPnPrRV/J5XuWsNE85UtrDSC7qxFA9lYHsqqnZAUZvtiDyQjm+t4EeTQEpR7d8ij75O1fgIxII16n1JDKBtlrnLylGJ+35KiccahhkFCiu1z3QVu3tV7XoWHuFNioZXJv6Gx/kOFtVqUzbltAhx4aL2zdhtutrwfZUyx2uFjwJyUGEjoGlrMSOLmG3tEDAMlYC1EX695VlPQBpX5A0eFPHsmmE23qdRINCREUzdsIcy4Wf1EjMJX29EL2drYPEGx/mYZbTZR7/tm8YJKZyEqgV8kn1boCX7wYMsd6jTHkWxd+6kklNo8z/gYrhlzP+xYEnOGBKXCLoiPXJQRsT31rdmKvHxKMAU4ZOjm9sjGPXMrVi824Cg7LRK9xQk5YQCookKKoMITOfH0MIkOeP6WAYfx9w7fdce0S5TzhoR9JcKKDRE1bJ57yk0eGCgo/fgsAXy4JvrFXJ8oXnukspY7T6XwmoOEFrpjkPAUsSDHAj87oto33K3u14CeF2EjTb/6aK2edz8NhDMzloVgPNZe/WGnM9JfzWzl3HtpDSnK9IMb6PrIb9xvR3Iw8QDl3cP4OZHoZFYkRl2ehvN6atojJ9wtlbSRIRJc2bct0duqtDVvRJ6gOH8n0ewEaxYXx2wtLM8qxsDz74Om1bGTdWCu4U3kVAaXSO022opMYmlnwYl08/QNSSbvQhgmtCmgeLgcSthtrYtpZTqI60TCWzWc7aRrOkC40fCa4D+o+pCa2WHdwwD8Rf3Nt/1RM8NszIj8Im61GgjBRMoU4XpUzL+m8LFYwfR9xX3X5iw1UwHQ==";
